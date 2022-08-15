847e2916498d03a786f762417cc9da98c03bbe79
Tiirth <tiirthdeora@gmail.com>
2022-08-15 16:10:24 +0530

    change

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
   reactStrictMode: true,
   images: {
     domains: ['pbs.twimg.com', 'lh3.googleusercontent.com']
